fd9f6ad97cb639d2f83e90c9237fe60e79a74f8f
aimanshakeel <aimanshakeel1008@gmail.com>
2024-10-31 18:39:33 +0530

    initial commit

---
+++ src/test/java/com/mealmap/mealmap_backend_api/MealmapBackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mealmap.mealmap_backend_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealmapBackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
